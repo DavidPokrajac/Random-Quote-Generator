2681c23e57f5ee84eca7b4fd0448542d7fe4c60a
DavidPokrajac <d0pokrajac@gmail.com>
2020-10-07 11:41:55 +0200

    Added event listener for button tag

--- sandbox.js
+++ sandbox.js
@@ -1,19 +1,28 @@
 window.onload = function(){
 
   // referencing button tag
-  var button = document.querySelector("button");
+  var button = document.querySelector(".container button");
 
-  var quote = document.getElementById("quote");
+  // referencing quote paragraph
+  var quoteA = document.getElementById("quote");
 
+  // referencing author paragraph
   var author = document.getElementById("author");
 
+  // Choosing random quotes and storing them in one array
   var quotes = [
-    {quote: "We are what we repeatedly do. Excellence, then, is not an act, but a habit", author: "Aristotle"},
-    {quote: "A good decision is based on knowledge and not on numbers". author: "Plato"},
-    {quote: "The only true wisdom is in knowing you know nothing", author: "Socrates"},
-    {quote: "Educate the children and it won't be necessary to punish the men", author: "Pythagoras"},
-    {quote: "Be silent unless you can say something that is more useful than your silence", author: "Archimedes"}
+    {quote: "We are what we repeatedly do. Excellence, then, is not an act, but a habit.", author: "Aristotle"},
+    {quote: "A good decision is based on knowledge and not on numbers.", author: "Plato"},
+    {quote: "The only true wisdom is in knowing you know nothing.", author: "Socrates"},
+    {quote: "Educate the children and it won't be necessary to punish the men.", author: "Pythagoras"},
+    {quote: "Be silent unless you can say something that is more useful than your silence.", author: "Archimedes"}
   ];
 
+  button.addEventListener("click", function(){
+    // Every time a button is clicked, new index variable will be generated so that it can be used as an index of a quote array
+    var index = Math.floor(Math.random() * quotes.length);
+    quoteA.textContent = quotes[index].quote;
+    author.textContent = quotes[index].author;
+  });
 
 }
