3d48cfd840b052063d1fa15daa3e1ecd8094375a
DavidPokrajac <d0pokrajac@gmail.com>
2020-10-07 10:35:45 +0200

    Added quotes in the array

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,19 @@
 window.onload = function(){
 
-  console.log("Test");
+  // referencing button tag
+  var button = document.querySelector("button");
+
+  var quote = document.getElementById("quote");
+
+  var author = document.getElementById("author");
+
+  var quotes = [
+    {quote: "We are what we repeatedly do. Excellence, then, is not an act, but a habit", author: "Aristotle"},
+    {quote: "A good decision is based on knowledge and not on numbers". author: "Plato"},
+    {quote: "The only true wisdom is in knowing you know nothing", author: "Socrates"},
+    {quote: "Educate the children and it won't be necessary to punish the men", author: "Pythagoras"},
+    {quote: "Be silent unless you can say something that is more useful than your silence", author: "Archimedes"}
+  ];
+
 
 }
